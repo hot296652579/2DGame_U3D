08c147fa812997c04df9b07c83b6e2d067a7354f
Javan <2808327878@qq.com>
2021-05-21 18:08:14 +0800

    fff

---
+++ Assets/Scripts/ScriptableObjects/GameSceneSO.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.AddressableAssets;
+using System.Collections;
+
+public class GameSceneso :ScriptableObject
+{
+    public GameSceneType sceneType;
+    public AssetReference assetReference;
+
+    public enum GameSceneType
+    {
+        Location,
+        Menu,
+        Initialisation,
+        PersistentManagers,
+        Gameplay,
+        Art
+    }
+}
